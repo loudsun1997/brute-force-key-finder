6439101d5932732324afd8652c770f549726deaa
Zhiwei Yang <zhiwei.yang1997@gmail.com>
2020-02-28 16:25:45 -0600

    d

--- fileOperation.c
+++ fileOperation.c
@@ -11,9 +11,12 @@ int main()
     {
         fprintf(stderr, "Could not open file\n");
     }
-    while(fgets(word, 16, fp))
+    for(int i = 0; i <10; i++)
     {
-        printf("word: %s", word);
-    }
+        while(fgets(word, 16, fp))
+        {
 
+            printf("word: %s", word);
+        }
+    }
 }
