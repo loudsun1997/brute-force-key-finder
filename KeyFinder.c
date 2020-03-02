9a8eb9ddc68fc8316954d6a853d104b1b2733077
Zhiwei Yang <zhiwei.yang1997@gmail.com>
2020-03-01 21:09:47 -0600

    Update KeyFinder.c

--- KeyFinder.c
+++ KeyFinder.c
@@ -53,7 +53,6 @@ int main (void)
 
     while ((read = getline(&buffer, &bufsize, fp)) != -1) 
     {
-       /* Buffer for the decrypted text */
        
        key = (unsigned char *)buffer;
     //    key=(unsigned char *)"nature##########";
@@ -127,49 +126,4 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
     EVP_CIPHER_CTX_free(ctx);
 
     return ciphertext_len;
-}
-
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *plaintext)
-{
-    EVP_CIPHER_CTX *ctx;
-
-    int len;
-
-    int plaintext_len;
-
-    /* Create and initialise the context */
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
-        handleErrors();
-
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        handleErrors();
-    plaintext_len = len;
-
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-        handleErrors();
-    plaintext_len += len;
-
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
 }
